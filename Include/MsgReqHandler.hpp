90b2427f99709547658a81c667b42b57146a7a77
pikwiatk <piotr.1.kwiatkowski@nokia.com>
2018-06-26 13:48:16 +0800

    adde typedefs for req amd resp in ReqHandler

--- Include/MsgReqHandler.hpp
+++ Include/MsgReqHandler.hpp
@@ -7,6 +7,9 @@ template <typename Req, typename Resp>
 class ReqHandler
 {
 public:
+	typedef Req RequestType;
+	typedef Resp ResponseType;
+
 	virtual ~ReqHandler() {}
 	virtual Resp handle(const Req& p_message) = 0;
 };
